5d23dcf3ad04be44ced3f658c114c13dfbaaa661
Aziz <64666452+httpsecure@users.noreply.github.com>
2022-09-12 16:10:53 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// Theme Toggle
+const themeToggle = document.querySelector(".theme-toggle");
+
+if (themeToggle) {
+  themeToggle.addEventListener("click", switchTheme, true);
+} else {
+  localStorage.removeItem("theme");
+}
+
+// Switch the theme
+function switchTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme == "dark") {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light")
+  } else {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark")
+  }
+}
